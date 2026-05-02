6bd4a1ffa4ad0b15ba17cbf31cac5092413f4473
Juli <laudate0419@gmail.com>
2026-05-03 01:21:12 +0900

    배포되랏

--- vite.config.ts
+++ vite.config.ts
@@ -7,14 +7,14 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    base: mode === 'production' ? '/bomflow/' : '/',
+    base: '/bomflow/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '.'),
+        '@': path.resolve(__dirname, './src'),
       },
     },
     server: {
