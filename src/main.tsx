3c391969a366ffb543c3f20e62f6ddd4d476ecde
Juli <laudate0419@gmail.com>
2026-05-03 01:11:16 +0900

    main.tsx 수정

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/bomflow">
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 );
